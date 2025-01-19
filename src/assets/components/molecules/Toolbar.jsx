45a0cf32f8ef54eb90f4753c804a5e15cc2e1a96
WGrot <117313232+WGrot@users.noreply.github.com>
2025-01-19 19:22:58 +0100

    added dark mode definitions

--- src/assets/components/molecules/Toolbar.jsx
+++ src/assets/components/molecules/Toolbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Toolbar.css';
+import {DarkModeToggle} from "../atoms/DarkModeButton.jsx";
 import SunIcon from '../atoms/Icons/SunIcon.svg';
 import MoonIcon from '../atoms/Icons/MoonIcon.svg';
 import LanguageIcon from '../atoms/Icons/LanguageIcon.svg';
@@ -19,8 +20,8 @@ const Toolbar = () => {
     return (
         <div className="toolbar">
             <div className="toolbar-content-left">
-                <img src={MoonIcon} alt="Sun Icon" width="20" height="20"/>
-                <img src={LanguageIcon} alt="Sun Icon" width="20" height="20"/>
+                <DarkModeToggle/>
+                <img src={LanguageIcon} alt="Sun Icon" width="30" height="30"/>
                 En
             </div>
 
