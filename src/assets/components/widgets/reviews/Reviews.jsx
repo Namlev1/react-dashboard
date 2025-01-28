1bd08b39bebc411e5dc32ef1dfab6d65892ca09f
namlev <mikipatkowski@protonmail.com>
2025-01-28 12:50:31 +0100

    Add likes and dislikes to reviews widget, improve styling

--- src/assets/components/widgets/reviews/Reviews.jsx
+++ src/assets/components/widgets/reviews/Reviews.jsx
@@ -1,10 +1,10 @@
 import WidgetTitleBar from '../../atoms/WidgetTitleBar/WidgetTitleBar.jsx'
-import styles from '../qualityOfSale/QualityOfSale.module.css'
 import Table from '../../molecules/Table/Table.jsx'
 import TableEntry from '../../molecules/Table/TableEntry.jsx'
 import { useSelector } from 'react-redux'
 import LikeIcon from '/src/assets/components/atoms/Icons/LikeIcon.svg'
 import DislikeIcon from '/src/assets/components/atoms/Icons/DislikeIcon.svg'
+import styles from './Reviews.module.css'
 
 const Reviews = () => {
   const data = useSelector((store) => store.accountSlice.reviews)
@@ -14,15 +14,27 @@ const Reviews = () => {
       <WidgetTitleBar text={'Reviews'} />
 
       <div className={'widget-content'}>
-        <div className={styles['score-container']}>
-          <div className={'widget-frame'}>
-            <p>{data.score}</p>
+        <div className={'widget-frame ' + styles['score-frame']}>
+          <div>
+            <p>
+              {data.score}%
+              <span className={styles['small-font']}> positive reviews</span>
+            </p>
           </div>
-          <div></div>
+          <div className={styles['score-frame-stats']}>
+            <div>
+              <img src={LikeIcon} alt={'Positive reviews'} />
+              <p style={{ color: 'var(--like)' }}>{data.positive}</p>
+            </div>
+            <div>
+              <img src={DislikeIcon} alt={'Negative reviews'} />
+              <p style={{ color: 'var(--dislike)' }}>{data.negative}</p>
+            </div>
+          </div>
         </div>
 
         <div>
-          <div className={'widget-frame'}>
+          <div className={'widget-frame ' + styles['review-table-frame']}>
             <h3>Most recent reviews</h3>
             <Table ratio={'10-5-2-13'}>
               {data.details.map((review) => (
