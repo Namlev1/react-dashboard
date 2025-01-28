10beead762764a6ae771077fcd171ec45bfef6e9
namlev <mikipatkowski@protonmail.com>
2025-01-28 11:32:57 +0100

    Add images as content for TableEntry

--- src/assets/components/molecules/Table/TableEntry.jsx
+++ src/assets/components/molecules/Table/TableEntry.jsx
@@ -5,7 +5,27 @@ const TableEntry = ({ content, gridTemplateColumns }) => {
     <div className={styles['table-content']} style={{ gridTemplateColumns }}>
       {content.map((field, idx) => {
         if (idx === 0) {
-          return <p key={field}>{field}</p> // don't render left border
+          // don't render left border
+          if (field.src) {
+            return (
+              <div key={field.src} className={'flex-center'}>
+                <img src={field.src} alt={field.alt} />
+              </div>
+            )
+          }
+          return <p key={field}>{field}</p>
+        }
+
+        if (field.src) {
+          return (
+            <div
+              key={field.src}
+              className={'flex-center'}
+              style={{ borderLeft: '1px solid var(--text)' }}
+            >
+              <img src={field.src} alt={field.alt} />
+            </div>
+          )
         }
         return (
           <p key={field} style={{ borderLeft: '1px solid var(--text)' }}>
